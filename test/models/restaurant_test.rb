4d12140fc3c4d9fc972970cdf28fff087f5071af
Mike Bastarache <mb00147@my.westga.edu>
2022-03-27 17:56:56 -0400

    Added restaurant test fixtures and tests for validation in model

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class RestaurantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @restaurant = restaurants(:one)
+  end
+
+  test "restaurant attributes must not be empty" do
+    restaurant = Restaurant.new
+    assert restaurant.invalid?
+    assert restaurant.errors[:name].any?
+    assert restaurant.errors[:city].any?
+    assert restaurant.errors[:state].any?
+  end
+
+  test "restaurant is not valid for non-unique name in a city and state" do
+    restaurant = Restaurant.new(
+                            name: @restaurant.name,
+                            city: "Boston",
+                            state:  "Massachusetts"
+                          )
+    assert restaurant.invalid?
+    assert_equal ["has already been taken"], restaurant.errors[:name]
+  end
 end
