16411e36deee14c0ebbe6d8f3a33359d9a4de952
Mike Bastarache <mb00147@my.westga.edu>
2022-03-27 18:01:35 -0400

    Added tests for restaurant model search

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -22,4 +22,74 @@ class RestaurantTest < ActiveSupport::TestCase
     assert restaurant.invalid?
     assert_equal ["has already been taken"], restaurant.errors[:name]
   end
+
+  test "search returns a single restaurant" do
+    restaurant = Restaurant.search(@restaurant.name, @restaurant.city)
+    assert_equal 1, restaurant.count
+  end
+
+  test "search returns multiple restaurants" do
+    restaurant = Restaurant.search("Test Restaurant", "Boston, Massachusetts")
+    assert restaurant.count > 1
+  end
+
+  test "search returns no restaurants" do
+    restaurant = Restaurant.search("Unknown Restaurant", "Atlanta, Georgia")
+    assert_equal 0, restaurant.count
+  end
+
+  test "search returns all restaurants if no search parameters specified" do
+    restaurant = Restaurant.search("", "")
+    assert_equal 52, restaurant.count
+  end
+
+  test "search finds restaurants by exact name without location" do
+    restaurant = Restaurant.search(@restaurant.name, "")
+    assert_equal 2, restaurant.count
+    assert_equal "Boston Eatery", restaurant.first.name
+  end
+
+  test "search finds restaurants by exact name and city" do
+    restaurant = Restaurant.search(@restaurant.name, @restaurant.city)
+    assert_equal 1, restaurant.count
+    assert_equal "Boston Eatery", restaurant.first.name
+    assert_equal "Boston", restaurant.first.city
+  end
+
+  test "search finds restaurants by exact name and state" do
+    restaurant = Restaurant.search(@restaurant.name, @restaurant.state)
+    assert_equal 1, restaurant.count
+    assert_equal "Boston Eatery", restaurant.first.name
+    assert_equal "Massachusetts", restaurant.first.state
+  end
+
+  test "search finds restaurant by exact name and city and state" do
+    location = @restaurant.city + ", " + @restaurant.state
+    restaurant = Restaurant.search(@restaurant.name, location)
+    assert_equal 1, restaurant.count
+    assert_equal "Boston Eatery", restaurant.first.name
+    assert_equal "Boston", restaurant.first.city
+    assert_equal "Massachusetts", restaurant.first.state
+  end
+
+  test "search finds restaurants by city only" do
+    restaurant = Restaurant.search("", @restaurant.city)
+    assert_equal 51, restaurant.count
+  end
+
+  test "search finds restaurants by state only" do
+    restaurant = Restaurant.search("", @restaurant.state)
+    assert_equal 51, restaurant.count
+  end
+
+  test "search finds restaurants by city and state only" do
+    location = @restaurant.city + ", " + @restaurant.state
+    restaurant = Restaurant.search("", location)
+    assert_equal 51, restaurant.count
+  end
+
+  test "search finds restaurants by partial name" do
+    restaurant = Restaurant.search("Test", "")
+    assert_equal 50, restaurant.count
+  end
 end
