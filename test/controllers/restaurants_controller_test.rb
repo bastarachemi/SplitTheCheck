3aa05fa44f9e122fd283dacf79274793a5e1ab5b
Mike Bastarache <mb00147@my.westga.edu>
2022-03-27 18:24:59 -0400

    Modified default restaurant controller tests

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -8,6 +8,8 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get restaurants_url
     assert_response :success
+    assert_select 'h1', 'Split The Check'
+    assert_select 'tbody tr', 10
   end
 
   test "should get new" do
@@ -16,6 +18,7 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create restaurant" do
+    @restaurant.name = "Different Restaurant"
     assert_difference('Restaurant.count') do
       post restaurants_url, params: { restaurant: { city: @restaurant.city, name: @restaurant.name, state: @restaurant.state, will_split: @restaurant.will_split, wont_split: @restaurant.wont_split } }
     end
@@ -26,11 +29,16 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   test "should show restaurant" do
     get restaurant_url(@restaurant)
     assert_response :success
+    assert_select 'h1', 'Split The Check'
+    assert_select 'h2', 'Restaurant Details'
+    assert_select '.btn', 3
   end
 
   test "should get edit" do
     get edit_restaurant_url(@restaurant)
     assert_response :success
+    assert_select 'h1', 'Split The Check'
+    assert_select 'h2', 'Editing Restaurant'
   end
 
   test "should update restaurant" do
@@ -38,11 +46,9 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to restaurant_url(@restaurant)
   end
 
-  test "should destroy restaurant" do
-    assert_difference('Restaurant.count', -1) do
+  test "cannot destroy restaurant" do
+    assert_raises(ActionController::RoutingError) do
       delete restaurant_url(@restaurant)
     end
-
-    assert_redirected_to restaurants_url
   end
 end
