44f3e433dfc7759048726f0fc30aa65f0c040922
Mike Bastarache <mb00147@my.westga.edu>
2022-03-27 23:40:34 -0400

    Final Sprint 1 Commit
    Modified system tests to pass

--- test/system/restaurants_test.rb
+++ test/system/restaurants_test.rb
@@ -7,45 +7,39 @@ class RestaurantsTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit restaurants_url
-    assert_selector "h1", text: "Restaurants"
+    assert_selector "h1", text: "Split The Check"
+    assert_selector "th", text: "Name"
+    assert_selector "th", text: "Location"
+    assert_selector "th", text: "Will split"
+    assert_selector "th", text: "Wont split"
   end
 
   test "creating a Restaurant" do
     visit restaurants_url
-    click_on "New Restaurant"
+    click_on "Add New Restaurant"
+    assert_selector "h2", text: "Adding New Restaurant"
 
     fill_in "City", with: @restaurant.city
-    fill_in "Name", with: @restaurant.name
+    fill_in "Name", with: "New Restaurant"
     fill_in "State", with: @restaurant.state
-    fill_in "Will split", with: @restaurant.will_split
-    fill_in "Wont split", with: @restaurant.wont_split
     click_on "Create Restaurant"
 
     assert_text "Restaurant was successfully created"
-    click_on "Back"
+    click_on "Back to Home"
   end
 
   test "updating a Restaurant" do
     visit restaurants_url
-    click_on "Edit", match: :first
+    click_on "Vote", match: :first
+    click_on "Edit Restaurant"
 
-    fill_in "City", with: @restaurant.city
+    fill_in "City", with: "New Restaurant"
     fill_in "Name", with: @restaurant.name
     fill_in "State", with: @restaurant.state
-    fill_in "Will split", with: @restaurant.will_split
-    fill_in "Wont split", with: @restaurant.wont_split
     click_on "Update Restaurant"
 
-    assert_text "Restaurant was successfully updated"
-    click_on "Back"
+    assert_text "Restaurant was successfully updated."
+    click_on "Back to Home"
   end
 
-  test "destroying a Restaurant" do
-    visit restaurants_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Restaurant was successfully destroyed"
-  end
 end
