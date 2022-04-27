5e11ad5d2080a68caed6f719c2c8b9d3a03f98f3
Mike Bastarache <mb00147@my.westga.edu>
2022-04-27 11:50:20 -0400

    Removed will_split and wont_split columns from restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_17_024701) do
+ActiveRecord::Schema.define(version: 2022_04_27_154918) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "city"
     t.string "state"
-    t.integer "will_split", default: 0
-    t.integer "wont_split", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
