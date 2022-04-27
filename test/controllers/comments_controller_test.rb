617daad85dd011eeccf9011fc2806222d3ef400d
Mike Bastarache <mb00147@my.westga.edu>
2022-04-27 18:10:59 -0400

    Added tests for comments controller and removed unused actions tests.

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,48 +1,42 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @comment = comments(:one)
+    @user = users(:one)
+    @restaurant = restaurants(:one)
   end
 
-  test "should get index" do
-    get comments_url
+  test "should get new if logged in" do
+    sign_in @user
+    get new_restaurant_comment_path(@restaurant)
     assert_response :success
   end
 
-  test "should get new" do
-    get new_comment_url
-    assert_response :success
+  test "should not get new if logged out" do
+    get new_restaurant_comment_path(@restaurant)
+    assert_redirected_to new_user_session_path
   end
 
-  test "should create comment" do
-    assert_difference('Comment.count') do
-      post comments_url, params: { comment: { message: @comment.message, restaurant_id: @comment.restaurant_id, user_id: @comment.user_id } }
+  test "should create comment if logged in" do
+    sign_in @user
+    get restaurant_url(@restaurant)
+    assert_difference('@restaurant.comments.count') do
+      post restaurant_comments_url(@restaurant), params: { comment: { message: @comment.message, restaurant_id: @comment.restaurant_id, user_id: @comment.user_id } }
     end
 
-    assert_redirected_to comment_url(Comment.last)
+    assert_redirected_to restaurant_url(@restaurant)
   end
 
-  test "should show comment" do
-    get comment_url(@comment)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_comment_url(@comment)
-    assert_response :success
-  end
-
-  test "should update comment" do
-    patch comment_url(@comment), params: { comment: { message: @comment.message, restaurant_id: @comment.restaurant_id, user_id: @comment.user_id } }
-    assert_redirected_to comment_url(@comment)
-  end
-
-  test "should destroy comment" do
-    assert_difference('Comment.count', -1) do
-      delete comment_url(@comment)
+  test "should not create comment if logged out" do
+    get restaurant_url(@restaurant)
+    assert_no_difference('@restaurant.comments.count') do
+      post restaurant_comments_url(@restaurant), params: { comment: { message: @comment.message, restaurant_id: @comment.restaurant_id, user_id: @comment.user_id } }
     end
 
-    assert_redirected_to comments_url
+    assert_redirected_to new_user_session_path
   end
+
 end
