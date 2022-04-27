dd414600756fa881ad52db0dd3cc28939d4119c6
Mike Bastarache <mb00147@my.westga.edu>
2022-04-27 13:30:58 -0400

    Generated Favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_27_154918) do
+ActiveRecord::Schema.define(version: 2022_04_27_172933) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_favorites_on_restaurant_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -42,6 +51,8 @@ ActiveRecord::Schema.define(version: 2022_04_27_154918) do
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
+  add_foreign_key "favorites", "restaurants"
+  add_foreign_key "favorites", "users"
   add_foreign_key "votes", "restaurants"
   add_foreign_key "votes", "users"
 end
